c8dda2d3ce65d14c3c068b803e92ca0a1d6b795f
Andrey Soldatov <qwer4132qwer@gmail.com>
2024-12-06 16:55:08 +0300

    Fixed circular import bug

--- Backend/app/routers/boards_db.py
+++ Backend/app/routers/boards_db.py
@@ -1,8 +1,25 @@
 from sqlmodel import Field, SQLModel, Relationship
-from columns_db import Column
-
 class Board(SQLModel, table=True):
     id: int = Field(primary_key=True)
     title: str = Field(max_length=20)
 
     columns: list["Column"] = Relationship(back_populates="board")
+
+class Column(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    board_id: int = Field(foreign_key="board.id")
+    title: str = Field(max_length=20)
+    ord_num: int = Field(default=0)
+
+    tasks: list["Task"] = Relationship(back_populates="column")
+    board: Board | None = Relationship(back_populates="columns")
+
+class Task(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    title: str = Field(max_length=20)
+    type: str = Field(max_length=20)
+    weight: int = Field(default=0)
+    ord_num: int = Field(max_length=20)
+    col_id: int = Field(foreign_key="column.id")
+
+    column: Column | None = Relationship(back_populates="tasks")
