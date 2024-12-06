eecca0cb71aebf86c98a54f02811279a03aba004
Andrey Soldatov <qwer4132qwer@gmail.com>
2024-12-06 16:06:31 +0300

    Add basic unit testing, and github action to run tests

---
+++ Backend/app/tests/test_auth.py
@@ -0,0 +1,38 @@
+from fastapi.testclient import TestClient
+from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel.pool import StaticPool
+
+from ..main import app
+from ..db import get_session
+
+import hashlib
+
+engine = engine = create_engine(
+        "sqlite://",  
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool,  
+    )
+SQLModel.metadata.create_all(engine)
+
+def get_session_override():
+    with Session(engine) as session:
+        return session
+
+app.dependency_overrides[get_session] = get_session_override
+client = TestClient(app)
+
+def test_reg():
+    resp = client.post("/auth/register", 
+        json={
+            "first_name": "Andrey", 
+            "last_name": "Soldatov", 
+            "login": "ross",
+            "password": "qwer4132"
+        })
+    assert resp.status_code == 201
+
+    hasher = hashlib.sha256()
+    hasher.update(("qwer4132" + resp.json().get("salt")).encode())
+
+    assert resp.json().get("password_hash") == hasher.hexdigest()
+    
